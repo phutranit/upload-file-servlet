943d734e76b9416b9e6dfc88c9088d23dd9e7732
Trần Phú <31065133+phutranit@users.noreply.github.com>
2017-10-07 21:54:30 +0700

    Add files via upload

---
+++ UploadFileController.java
@@ -0,0 +1,61 @@
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+/**
+* author: Phu Tran
+* https://itphutran.com
+*/
+@MultipartConfig(fileSizeThreshold=1024*1024*5, //5MB
+		maxFileSize=1024*1024*6, //6MB
+		maxRequestSize=1024*1024*7) // 7MB
+public class UploadController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public UploadController() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		String status = request.getParameter("status");
+		final Part part = request.getPart("image");
+		// l?y tên file
+		String filename = getFileName(part);
+		
+		// du?ng d?n luu file
+		final String path = request.getServletContext().getRealPath("/files");
+		System.out.println(path);
+		File dirPath = new File(path);
+		if (!dirPath.exists()) {
+			dirPath.mkdir();
+		}
+		
+		// ghi file
+		String filePath = path + File.separator + filename;
+		part.write(filePath);
+	}
+	
+	private String getFileName(final Part part) {
+	    final String partHeader = part.getHeader("content-disposition");
+	    for (String content : partHeader.split(";")) {
+	        if (content.trim().startsWith("filename")) {
+	            return content.substring(
+	                    content.indexOf('=') + 1).trim().replace("\"", "");
+	        }
+	    }
+	    return null;
+	}
+
+}
